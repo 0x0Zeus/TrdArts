8d6f4e6ddddf4314e83e35a340522458bd115977
Codeus0624 <10914960+Codeus0624@users.noreply.github.com>
2025-05-26 21:11:11 +0900

    feat: complete responsive

--- src/page/ArticleDetail.tsx
+++ src/page/ArticleDetail.tsx
@@ -5,6 +5,8 @@ import moment from "moment";
 import axios from "axios";
 import { basic_url } from "@/stack/stack";
 import SubscribeModal from "@/components/SubscribeModal";
+import parse from "html-react-parser";
+import Seperator from "@/components/Seperator";
 
 interface DataItem {
   category_id: number;
@@ -24,7 +26,7 @@ const ArticleDetail = () => {
 
   const handleSubscribe = () => {
     setSubscribeVisible(true);
-  }
+  };
 
   useEffect(() => {
     if (id.id !== undefined) {
@@ -37,27 +39,27 @@ const ArticleDetail = () => {
         const contents = responseData?.contents;
         const splited_array = contents?.split("<h2>");
         let content = "";
-        for (var i = 0; i < splited_array?.length; i ++) {
+        for (var i = 0; i < splited_array?.length; i++) {
           content += splited_array[i];
           content += "<h2>";
         }
         responseData["contents"] = content;
-        setDetailData(res.data)
-      })
+        setDetailData(res.data);
+      });
     }
-  }, [])
+  }, []);
 
   return (
     <>
       <div className="container">
-        <div className="mb-3 mt-[100px] text-4xl font-bold">
+        <div className="mb-3 mt-[100px] text-2xl md:text-4xl font-bold">
           {detailData?.title}
         </div>
-        <div className="flex items-center text-xl text-black">
+        <div className="my-5 flex items-center text-xl text-[#FFFFFF]/80">
           <span className="pr-2">By :</span>
-          <span className="pr-2 font-bold">TRDARTS.com</span>
-          <div className="h-10 w-10 overflow-hidden rounded-[50%]">
-            <img src="/icons/icon.png" className="h-10 w-10"></img>
+          <span className="pr-2 font-bold">TrdARTS.com</span>
+          <div className="h-6 w-6 overflow-hidden rounded-[50%]">
+            <img src="/icons/icon.png" className="h-6 w-6"></img>
           </div>
         </div>
         <div className="mb-3 text-[18px] text-gray-600">
@@ -67,17 +69,19 @@ const ArticleDetail = () => {
             GMP-5
           </span>
         </div>
-        <hr className="pb-3" />
+        <Seperator />
         <div className="article-content">
-          {detailData !== undefined && parseFloat(detailData.contents)}
+          {detailData !== undefined && parse(detailData.contents)}
         </div>
-        <div className="my-10 flex flex-col items-center justify-center gap-5 border-b border-t border-[#006F99] py-5 font-mono sm:flex-row">
-          <p className="text-center text-base text-black xl:text-xl">
+        <Seperator />
+        <div className=" flex flex-col items-center justify-center gap-5  py-5 font-mono sm:flex-row">
+          <p className="text-center text-base text-[#FFFFFF]/80 xl:text-xl">
             Don't miss a thing! Sign up for a daily update delivered to your
-            inbox.
+            inbox
           </p>
           <button
-            className="h-[40px] w-[200px] bg-[#006F99] text-base text-white"
+            type="submit"
+            className="h-[46px] min-w-[160px] rounded-lg bg-[#8B37F4] text-base text-white"
             onClick={() => handleSubscribe()}
           >
             Subscribe
@@ -86,7 +90,7 @@ const ArticleDetail = () => {
         <SubscribeModal
           visible={subscribeVisible}
           setVisible={setSubscribeVisible}
-        />
+        ></SubscribeModal>
       </div>
     </>
   );
